e1317e81bb8b136fb7b08edd134eeb6cd84808d2
Floating-Ocean <sea113290980@qq.com>
2025-06-01 00:04:19 +0800

    fix(Number-Guess): 修复小bug

--- src/module/number_guess.py
+++ src/module/number_guess.py
@@ -1,4 +1,5 @@
 import random
+import threading
 from dataclasses import dataclass
 from enum import Enum
 
@@ -7,7 +8,7 @@ from src.core.bot.message import RobotMessage
 from src.core.constants import Constants
 from src.core.util.tools import check_is_int
 
-__number_guess_version__ = "v1.0.0"
+__number_guess_version__ = "v1.0.1"
 
 
 def register_module():
@@ -30,6 +31,7 @@ class GuessInfo:
 
 
 _guess_info = {}
+_guess_info_lock = threading.Lock()
 
 
 @command(tokens=["guess", "猜数字"])
@@ -39,62 +41,60 @@ def reply_number_guess(message: RobotMessage):
                       modal_words=False)
         return None
 
-    current_uuid = message.uuid
-    if current_uuid not in _guess_info:
-        _guess_info[current_uuid] = GuessInfo(GuessStatus.IDLE, -1, (-1, -1), -1)
+    with _guess_info_lock:
+        current_uuid = message.uuid
+        if current_uuid not in _guess_info:
+            _guess_info[current_uuid] = GuessInfo(GuessStatus.IDLE, -1, (-1, -1), -1)
 
-    if len(message.tokens) == 1:
-        if _guess_info[current_uuid].status == GuessStatus.RUNNING:
-            message.reply("游戏已经开始，请使用 \"/guess [num]\" 猜测数字，不要带上中括号")
-            return None
+        if len(message.tokens) == 1:
+            if _guess_info[current_uuid].status == GuessStatus.RUNNING:
+                message.reply("游戏已经开始，请使用 \"/guess [num]\" 猜测数字，不要带上中括号")
+                return None
 
-        range_min, range_max = 0, 0
-        while range_max - range_min < 100:
-            range_min, range_max = random.randint(1, 50000), random.randint(1, 50000)
-            if range_max < range_min:
-                range_min, range_max = range_max, range_min
+            range_min = random.randint(1, 49900)  # 确保至少有100的范围
+            range_max = random.randint(range_min + 100, 50000)
 
-        target = random.randint(range_min, range_max)
-        _guess_info[current_uuid] = GuessInfo(GuessStatus.RUNNING, target, (1, range_max), 0)
-        message.reply("猜数字开始！使用 \"/guess [num]\" 猜测数字，不要带上中括号\n\n"
-                      f"目标位于闭区间 [{range_min}, {range_max}] 内")
+            target = random.randint(range_min, range_max)
+            _guess_info[current_uuid] = GuessInfo(GuessStatus.RUNNING, target, (range_min, range_max), 0)
+            message.reply("猜数字开始！使用 \"/guess [num]\" 猜测数字，不要带上中括号\n\n"
+                          f"目标位于闭区间 [{range_min}, {range_max}] 内")
 
-    else:
-        if _guess_info[current_uuid].status == GuessStatus.IDLE:
-            message.reply(f"游戏还未开始\n\n{Constants.help_contents['number-guess']}",
-                          modal_words=False)
-            return None
-
-        if _guess_info[current_uuid].status == GuessStatus.ENDED:
-            message.reply(f"上一轮游戏已结束\n\n{Constants.help_contents['number-guess']}",
-                          modal_words=False)
-            return None
-
-        participant_guess_plain = message.tokens[1]
-
-        if not check_is_int(participant_guess_plain):
-            message.reply("参数格式错误，请输入数字")
-            return None
-
-        participant_guess = int(participant_guess_plain)
-
-        current_info: GuessInfo = _guess_info[current_uuid]
-        if participant_guess == current_info.target:
-            message.reply(f"恭喜你猜对了，答案是 {participant_guess}，总共猜了 {current_info.trials} 次")
-            current_info = GuessInfo(GuessStatus.ENDED, -1, (-1, -1), -1)
-        elif participant_guess > current_info.target:
-            current_info.bound = (current_info.bound[0], min(participant_guess - 1, current_info.bound[1]))
-            current_info.trials += 1
-            message.reply(f"太大了！\n\n"
-                          f"目标位于闭区间 [{current_info.bound[0]}, {current_info.bound[1]}] 内，"
-                          f"目前总共猜了 {current_info.trials} 次")
         else:
-            current_info.bound = (max(participant_guess + 1, current_info.bound[0]), current_info.bound[1])
-            current_info.trials += 1
-            message.reply(f"太小了！\n\n"
-                          f"目标位于闭区间 [{current_info.bound[0]}, {current_info.bound[1]}] 内，"
-                          f"目前总共猜了 {current_info.trials} 次")
+            if _guess_info[current_uuid].status == GuessStatus.IDLE:
+                message.reply(f"游戏还未开始\n\n{Constants.help_contents['number-guess']}",
+                              modal_words=False)
+                return None
 
-        _guess_info[current_uuid] = current_info
+            if _guess_info[current_uuid].status == GuessStatus.ENDED:
+                message.reply(f"上一轮游戏已结束\n\n{Constants.help_contents['number-guess']}",
+                              modal_words=False)
+                return None
 
-    return None
+            participant_guess_plain = message.tokens[1]
+
+            if not check_is_int(participant_guess_plain):
+                message.reply("参数格式错误，请输入数字")
+                return None
+
+            participant_guess = int(participant_guess_plain)
+
+            current_info: GuessInfo = _guess_info[current_uuid]
+            if participant_guess == current_info.target:
+                message.reply(f"恭喜你猜对了，答案是 {participant_guess}，总共猜了 {current_info.trials} 次")
+                current_info = GuessInfo(GuessStatus.ENDED, -1, (-1, -1), -1)
+            elif participant_guess > current_info.target:
+                current_info.bound = (current_info.bound[0], min(participant_guess - 1, current_info.bound[1]))
+                current_info.trials += 1
+                message.reply(f"太大了！\n\n"
+                              f"目标位于闭区间 [{current_info.bound[0]}, {current_info.bound[1]}] 内，"
+                              f"目前总共猜了 {current_info.trials} 次")
+            else:
+                current_info.bound = (max(participant_guess + 1, current_info.bound[0]), current_info.bound[1])
+                current_info.trials += 1
+                message.reply(f"太小了！\n\n"
+                              f"目标位于闭区间 [{current_info.bound[0]}, {current_info.bound[1]}] 内，"
+                              f"目前总共猜了 {current_info.trials} 次")
+
+            _guess_info[current_uuid] = current_info
+
+        return None
