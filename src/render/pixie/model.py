5ee637de5370d556c489f955744c1a73f3c983bb
Floating-Ocean <sea113290980@qq.com>
2025-07-10 16:23:38 +0800

    refactor(Renderer): 统一成员命名规范

--- src/render/pixie/model.py
+++ src/render/pixie/model.py
@@ -13,7 +13,10 @@ _img_load_cache: dict[str, tuple[float, pixie.Image]] = {}
 
 
 class Renderer(abc.ABC):
-    """图片渲染基类"""
+    """
+    图片渲染基类
+    成员命名规范：渲染组件公开，可用命名前缀: str_, img_, section_；中间量私有
+    """
 
     @abstractmethod
     def render(self) -> pixie.Image:
@@ -77,50 +80,50 @@ class RenderableSvgSection(RenderableSection, abc.ABC):
         pass
 
     def __init__(self, svg_ts_path: str, width: int = -1, height: int = -1):
-        self.svg_ts_path = f'{svg_ts_path}.svg'
-        tetris_svg, self.original_width, self.original_height = self._generate_svg()
-        with open(self.svg_ts_path, 'w') as f:
-            f.write(tetris_svg)
-        self.tetris_map = pixie.read_image(self.svg_ts_path)
+        svg_ts_path = f'{svg_ts_path}.svg'
+        svg, self._original_width, self._original_height = self._generate_svg()
+        with open(svg_ts_path, 'w') as f:
+            f.write(svg)
+        self.img_svg = pixie.read_image(svg_ts_path)
 
         # 计算目标尺寸和居中偏移
         self._calculate_dimensions(width, height)
 
     def _calculate_dimensions(self, target_width, target_height):
-        aspect_ratio = self.original_width / self.original_height
+        aspect_ratio = self._original_width / self._original_height
 
         if target_width == -1 and target_height == -1:
             target_width = self._get_max_width()
 
         if target_width == -1:
-            self.render_width = int(target_height * aspect_ratio)
-            self.render_height = target_height
+            self._render_width = int(target_height * aspect_ratio)
+            self._render_height = target_height
         elif target_height == -1:
-            self.render_width = target_width
-            self.render_height = int(target_width / aspect_ratio)
+            self._render_width = target_width
+            self._render_height = int(target_width / aspect_ratio)
         else:
             # 同时指定宽高时，居中显示而非拉伸
             container_ratio = target_width / target_height
             if aspect_ratio > container_ratio:
-                self.render_width = target_width
-                self.render_height = int(target_width / aspect_ratio)
+                self._render_width = target_width
+                self._render_height = int(target_width / aspect_ratio)
             else:
-                self.render_height = target_height
-                self.render_width = int(target_height * aspect_ratio)
+                self._render_height = target_height
+                self._render_width = int(target_height * aspect_ratio)
 
-        self.container_width = target_width if target_width != -1 else self.render_width
-        self.container_height = target_height if target_height != -1 else self.render_height
+        self._container_width = target_width if target_width != -1 else self._render_width
+        self._container_height = target_height if target_height != -1 else self._render_height
 
     def render(self, img: pixie.Image, x: int, y: int) -> int:
         current_x, current_y = x, y
-        current_x += (self.container_width - self.render_width) // 2
-        current_y += (self.container_height - self.render_height) // 2
+        current_x += (self._container_width - self._render_width) // 2
+        current_y += (self._container_height - self._render_height) // 2
 
-        draw_img(img, self.tetris_map,
-                 Loc(current_x, current_y, self.render_width, self.render_height))
-        current_y = y + self.container_height
+        draw_img(img, self.img_svg,
+                 Loc(current_x, current_y, self._render_width, self._render_height))
+        current_y = y + self._container_height
 
         return current_y
 
     def get_height(self):
-        return self.container_height
+        return self._container_height
