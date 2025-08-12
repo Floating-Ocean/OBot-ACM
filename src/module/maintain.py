4a3f1365cdb1bb6c55a5ba8f147eb72a7acfc9b6
Zhanyuan Guo <2364319479@qq.com>
2025-08-13 01:37:35 +0800

    refactor(robot.py): 将原 bot 入口文件拆分为 nonebot 入口文件+两个子模块 + cron.py 定时模块，包含原每日更新榜单和 (Onebot Only) 心跳模块 + maintain.py 杂项，主要用于腾讯官方机器人获取用户 ID 进行加权操作等。

---
+++ src/module/maintain.py
@@ -0,0 +1,25 @@
+from nonebot import on_command
+from nonebot.permission import SUPERUSER
+from nonebot.rule import to_me
+from nonebot.adapters import Event
+
+from src.core.constants import Constants
+from nonebot_plugin_saa import MessageFactory
+
+
+reload_conf = on_command("reload_conf", aliases={"配置重载"}, permission=SUPERUSER, rule=to_me(),priority=0,block=True)
+@reload_conf.handle()
+async def reply_reload_conf():
+    Constants.reload_conf()
+    Constants.log.info("[obot-core] 已重载配置文件")
+    await MessageFactory("已重载配置文件").finish()
+
+chat_scene_id = on_command("chat_scene_id", aliases={"对话场景ID"}, rule=to_me(),priority=5,block=True)
+@chat_scene_id.handle()
+async def reply_chat_scene_id(event:Event):
+    await MessageFactory(f'当前对话场景ID\n\n{event.get_session_id()}').finish()
+
+my_id = on_command("my_id", aliases={"我的ID"}, rule=to_me(),priority=5,block=True)
+@my_id.handle()
+async def reply_my_id(event:Event):
+    await MessageFactory(f'你的ID（不同对话场景下你的ID是不同的）\n\n{event.get_user_id()}').finish()
