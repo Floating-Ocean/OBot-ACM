73d283d0bba00f5b19ecbab8a833e1ca3ab61d32
Floating-Ocean <sea113290980@qq.com>
2025-09-23 23:56:24 +0800

    feat(ContestantFinder): 支持简单的 CPCFinder 查询

---
+++ src/platform/collect/cpcfinder.py
@@ -0,0 +1,92 @@
+from dataclasses import dataclass
+
+from src.core.util.tools import fetch_url_json
+
+
+@dataclass
+class CPCStudent:
+    id: str
+    name: str
+    school: str
+    champion: int
+    sec: int
+    thi: int
+    gold: int
+    silver: int
+    bronze: int
+
+
+@dataclass
+class CPCAward:
+    id: int
+    contest_id: int
+    contest_name: str
+    team_id: int
+    team_name: str
+    date: str
+    place: str
+    rank: int
+    official: bool
+    official_rank: int
+    medal: str
+
+
+_MEDAL_TYPE = {
+    'NONE': '铁牌',
+    'BRONZE': '铜牌',
+    'SILVER': '银牌',
+    'GOLD': '金牌',
+}
+
+
+class CPCFinder:
+
+    @classmethod
+    def find_student_id(cls, name: str, school: str)-> str | int:
+        json_data = fetch_url_json(f"https://cpcfinder.com/api/student?"
+                                   f"name={name}&school={school}",
+                                   throw=False, method='get')
+
+        if isinstance(json_data, int):
+            return -1
+
+        if len(json_data['data']) == 0:
+            return 0
+        if len(json_data['data']) > 1:
+            return 1
+
+        stu = json_data['data'][0]
+        return stu['studentId']
+
+    @classmethod
+    def get_student_general(cls, student_id: str) -> CPCStudent | int:
+        json_data = fetch_url_json(f"https://cpcfinder.com/api/student/{student_id}",
+                                   throw=False, method='get')
+
+        if isinstance(json_data, int):
+            return -1
+
+        stu = json_data['data']
+        return CPCStudent(stu['studentId'], stu['name'], stu['schoolName'],
+                          stu['championCount'], stu['secCount'], stu['thiCount'],
+                          stu['goldCount'], stu['silverCount'], stu['bronzeCount'])
+
+    @classmethod
+    def get_student_awards(cls, student_id: str) -> list[CPCAward] | int:
+        json_data = fetch_url_json(f"https://cpcfinder.com/api/student/{student_id}/awards",
+                                   throw=False, method='get')
+
+        if isinstance(json_data, int):
+            return -1
+
+        awards = json_data['data']
+        return [
+            CPCAward(award['awardId'], award['contestId'], award['contestName'],
+                     award['teamId'], award['teamName'],
+                     award['date'], award['place'], award['rank'],
+                     award['official'], award['officialRank'] if award['official'] else -1,
+                     _MEDAL_TYPE[award['medalType']] if award['official'] else '打星')
+            for award in awards
+        ]
+
+
