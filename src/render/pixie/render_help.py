8abed34b1c851088128b67b262b5392674f94ac9
Floating-Ocean <sea113290980@qq.com>
2026-08-16 12:18:34 +0800

    feat(Renderer): 帮助图片调暗背景、提亮主题色并新增渐隐分隔线

--- src/render/pixie/render_help.py
+++ src/render/pixie/render_help.py
@@ -2,7 +2,8 @@ import re
 
 import pixie
 from easy_pixie import StyledString, calculate_height, draw_text, calculate_width, Loc, draw_img, \
-    tuple_to_color, change_alpha, hex_to_color, lighten_color, draw_mask_rect
+    tuple_to_color, change_alpha, hex_to_color, lighten_color, draw_mask_rect, GradientColor, \
+    GradientDirection, draw_gradient_rect
 
 from src.core.constants import Constants, Help
 from src.render.pixie.model import Renderer, RenderableSection, SimpleCardRenderer
@@ -11,6 +12,13 @@ _CONTENT_WIDTH = 916
 _COLUMN_PADDING = 192
 _HELP_ITEM_PADDING = 64
 _HELP_CATEGORY_PADDING = 136
+_ACCENT_LIGHT_RATIO = 0.88
+_LINE_ALPHA = 100
+
+_CATEGORY_CHIP_PADDING_HORIZONTAL = 26
+_CATEGORY_CHIP_PADDING_VERTICAL = 9
+_RESTRICTION_CHIP_PADDING_HORIZONTAL = 26
+_RESTRICTION_CHIP_PADDING_VERTICAL = 12
 
 _CATEGORY_NAMES = {
     'Main': '榜单·比赛',
@@ -46,7 +54,7 @@ class _HelpItem(RenderableSection):
 
     def __init__(self, single_help: Help, accent_color: str):
         self._help = single_help
-        accent_light_color = lighten_color(hex_to_color(accent_color), 0.8)
+        accent_light_color = lighten_color(hex_to_color(accent_color), _ACCENT_LIGHT_RATIO)
 
         # 拆出限制短语，剩余内容作为描述展示
         self._restriction_text = None
@@ -73,8 +81,10 @@ class _HelpItem(RenderableSection):
         self.str_restriction = (StyledString(self._restriction_text, 'B', 26,
                                              font_color=(255, 222, 170)) if self._restriction_text
                                 else None)
-        self._restriction_chip_height = 8 * 2 + (self.str_restriction.height if self.str_restriction else 0)
-        self._restriction_chip_width = (16 * 2 + int(calculate_width(self.str_restriction))
+        self._restriction_chip_height = (_RESTRICTION_CHIP_PADDING_VERTICAL * 2 +
+                                         (self.str_restriction.height if self.str_restriction else 0))
+        self._restriction_chip_width = (_RESTRICTION_CHIP_PADDING_HORIZONTAL * 2 +
+                                        int(calculate_width(self.str_restriction))
                                         if self.str_restriction else 0)
 
         self.str_help = StyledString(
@@ -107,7 +117,7 @@ class _HelpItem(RenderableSection):
             draw_mask_rect(img, Loc(chip_x, chip_y, self._restriction_chip_width,
                                     self._restriction_chip_height),
                            (255, 222, 170, 44), self._restriction_chip_height // 2)
-            draw_text(img, self.str_restriction, chip_x + 16,
+            draw_text(img, self.str_restriction, chip_x + _RESTRICTION_CHIP_PADDING_HORIZONTAL,
                       chip_y + (self._restriction_chip_height - self.str_restriction.height) // 2)
 
         for line in self._lines:
@@ -126,13 +136,16 @@ class _HelpCategory(RenderableSection):
     """分类色块标题 + 该分类下的指令列表"""
 
     def __init__(self, category_name: str, helps: list[Help], accent_color: str):
-        accent_light_color = lighten_color(hex_to_color(accent_color), 0.8)
+        accent_light_color = lighten_color(hex_to_color(accent_color), _ACCENT_LIGHT_RATIO)
         accent_light_color_tran = change_alpha(accent_light_color, 48)
 
         self.str_title = StyledString(category_name, 'H', 36, font_color=accent_light_color)
-        self._chip_height = self.str_title.height + 12
-        self._chip_width = int(calculate_width(self.str_title)) + 28
+        self._chip_height = self.str_title.height + _CATEGORY_CHIP_PADDING_VERTICAL * 2
+        self._chip_width = int(calculate_width(self.str_title)) + _CATEGORY_CHIP_PADDING_HORIZONTAL * 2
         self._chip_color = accent_light_color_tran
+        # 标题右侧的渐隐线
+        self._line_color = change_alpha(accent_light_color, _LINE_ALPHA)
+        self._line_color_tran = change_alpha(accent_light_color, 0)
 
         self.section_items = [_HelpItem(single_help, accent_color) for single_help in helps]
 
@@ -146,8 +159,13 @@ class _HelpCategory(RenderableSection):
 
         draw_mask_rect(img, Loc(current_x, current_y, self._chip_width, self._chip_height),
                        self._chip_color, self._chip_height // 2)
-        draw_text(img, self.str_title, current_x + 14,
+        draw_text(img, self.str_title, current_x + _CATEGORY_CHIP_PADDING_HORIZONTAL,
                   current_y + (self._chip_height - self.str_title.height) // 2)
+        draw_gradient_rect(img, Loc(current_x + self._chip_width + 16,
+                                    current_y + self._chip_height // 2 - 1,
+                                    _CONTENT_WIDTH - self._chip_width - 16, 2),
+                           GradientColor([self._line_color, self._line_color_tran], [0.0, 1.0], ''),
+                           GradientDirection.HORIZONTAL, 1)
 
         current_y = y + self._chip_height + 48
         current_y -= _HELP_ITEM_PADDING
@@ -196,7 +214,7 @@ class _HelpSection(RenderableSection):
 class _TitleSection(RenderableSection):
 
     def __init__(self, accent_color: str):
-        accent_light_color = lighten_color(hex_to_color(accent_color), 0.8)
+        accent_light_color = lighten_color(hex_to_color(accent_color), _ACCENT_LIGHT_RATIO)
         accent_light_color_tran = change_alpha(accent_light_color, 136)
         self.img_help = Renderer.load_img_resource("Help", accent_light_color)
 
@@ -222,8 +240,13 @@ class _TitleSection(RenderableSection):
 
 class _CopyrightSection(RenderableSection):
 
-    def __init__(self, gradient_color_name: str):
+    def __init__(self, gradient_color_name: str, accent_color: str):
         mild_text_color = (255, 255, 255, 156)
+        accent_light_color = lighten_color(hex_to_color(accent_color), _ACCENT_LIGHT_RATIO)
+        # 顶部与内容区分隔的渐隐线
+        self._line_color = change_alpha(accent_light_color, _LINE_ALPHA)
+        self._line_color_tran = change_alpha(accent_light_color, 0)
+        self._line_gap = 26
 
         self.str_tips_title = StyledString(
             "Tips:", 'H', 36, padding_bottom=64, font_color=(255, 255, 255, 228)
@@ -245,6 +268,11 @@ class _CopyrightSection(RenderableSection):
     def render(self, img: pixie.Image, x: int, y: int) -> int:
         current_x, current_y = x, y
 
+        draw_gradient_rect(img, Loc(current_x, current_y, _CONTENT_WIDTH, 2),
+                           GradientColor([self._line_color, self._line_color_tran], [0.0, 1.0], ''),
+                           GradientDirection.HORIZONTAL, 1)
+        current_y += self._line_gap
+
         draw_text(img, self.str_tips_title, current_x, current_y)
         current_y = draw_text(img, self.str_tips_detail,
                               current_x + calculate_width(self.str_tips_title) + 12,
@@ -255,8 +283,9 @@ class _CopyrightSection(RenderableSection):
         return current_y
 
     def get_height(self):
-        return calculate_height([self.str_tips_title,
-                                 self.str_generator, self.str_generation_info])
+        return (self._line_gap +
+                calculate_height([self.str_tips_title,
+                                  self.str_generator, self.str_generation_info]))
 
 
 class HelpRenderer(SimpleCardRenderer):
@@ -271,11 +300,12 @@ class HelpRenderer(SimpleCardRenderer):
 
     @classmethod
     def _get_mask_color(cls) -> pixie.Color:
-        return tuple_to_color((12, 12, 12, 198))
+        return tuple_to_color((9, 9, 9, 226))
 
     def _get_render_sections(self) -> list[RenderableSection]:
         section_title = _TitleSection(self._gradient_color.color_list[-1])
         section_help = _HelpSection(Constants.help_contents, self._gradient_color.color_list[-1])
-        section_copyright = _CopyrightSection(self._gradient_color.name)
+        section_copyright = _CopyrightSection(self._gradient_color.name,
+                                              self._gradient_color.color_list[-1])
 
         return [section_title, section_help, section_copyright]
